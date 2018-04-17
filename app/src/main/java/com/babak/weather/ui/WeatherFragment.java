756dac5ab57a9152f4d742bdcdc1e6dd8848cc74
flaming-dubstep <sniders_@hotmail.com>
2018-04-17 13:57:37 +0200

    Design/layout for the fragment that will display the weather.

---
+++ app/src/main/java/com/babak/weather/ui/WeatherFragment.java
@@ -0,0 +1,28 @@
+package com.babak.weather.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.babak.weather.R;
+
+
+public class WeatherFragment extends Fragment {
+
+
+    public WeatherFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_weather, container, false);
+
+        
+    }
+
+}
