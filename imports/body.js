c595e64bc555992ee2ca119aa8549ab8d2c5d233
asaisacson <asaisacson@gmail.com>
2018-10-11 10:32:00 +0200

    test API calls

--- imports/body.js
+++ imports/body.js
@@ -7,30 +7,89 @@ import './body.html';
 
    // Sets the query to match which icon the user has clicked
    this.query = new ReactiveVar();
+   this.city = new ReactiveVar();
    this.showSun = new ReactiveVar(false);
    this.showRain = new ReactiveVar(false);
    this.showWind = new ReactiveVar(false);
    this.showCloud = new ReactiveVar(false);
    this.showSnow = new ReactiveVar(false);
+
+   this.cityData = new ReactiveVar();
+   this.cityPic = new ReactiveVar();
  });
+
+ // performSearch = (query = 'sun') => {
+ //   Promise.all([
+ //     axios.get('https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=9b5e8856a8ad837d711bf4c74b6e0ecf'),
+ //     axios.get('https://api.unsplash.com/search/photos/?page=1&per_page=10&query=${query}&client_id=bf54ced5af6f6f686d571a602ff4611ebf8fc2ebdc59989cdf768941e85148bb')
+ //   ])
+ //   // use arrow function to avoid loosing context
+ //   .then(([weatherResponse, picsResponse]) => {
+ //     console.log('weatherResponse.data', weatherResponse.data)
+ //     console.log('picsResponse.data.results', picsResponse.data.results)
+ //           this.setState({weather: weatherResponse.data, pics: picsResponse.data.results});
+ //     })
+ // 		.catch(err => {
+ // 			console.log('Error happened during fetching!', err);
+ // 		});
+ // }
+
+
+//API calls
+// 1. get current location (enter city)
+// 2. return a) curr city b) curr degrees c) curr weather picsResponse
+// 3. search function that takes query indicating weather (sun, snow, rain...)(to be tied to on click for when user clicks a weather icon) and returns data cities matching
+
+function wishCitiesWeather(weather) {
+   $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=9b5e8856a8ad837d711bf4c74b6e0ecf', function (data) {
+     var rawJson = JSON.stringify(data);
+     var json = JSON.parse(rawJson);
+     console.dir(json); //Update Weather parameters
+     this.cityData.set(json);
+   });
+ };
+
+function currentCityPic() {
+   let city = Template.instance().city.get();
+   $.getJSON('https://api.unsplash.com/search/photos/?page=1&per_page=1&query=' + city + '&client_id=bf54ced5af6f6f686d571a602ff4611ebf8fc2ebdc59989cdf768941e85148bb', function (data) {
+     var rawJson = JSON.stringify(data);
+     var json = JSON.parse(rawJson);
+     console.log('---------currentCityPic gets this city: ----------------', city)
+     console.log('---------currentCityPic returns: ----------------', json)
+
+     Template.instance().cityPic.set(json.results["0"].urls.regular);
+   });
+ };
+
+function currentCityWeather() {
+   let city = Template.instance().city.get();
+   $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=9b5e8856a8ad837d711bf4c74b6e0ecf', function (data) {
+     var rawJson = JSON.stringify(data);
+     var json = JSON.parse(rawJson);
+     console.log('---------currentCityWeather returns: ----------------', json)
+     Template.instance().cityData.set(json);
+   });
+ };
+
 
 
 Template.body.helpers({
 
- //returns data for the user's current city
  currCityData() {
+   const data = Template.instance().cityData.get();
+   const pic = Template.instance().cityPic.get();
+   console.log('------data----', data)
+   console.log('------pic----', pic)
    return [
-     { name: 'Stockholm',
-       temp: '23degrees',
-       icon: 'wi wi-day-sunny',
-       pic: 'https://images.unsplash.com/photo-1535403142432-2c6aaca60f79?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=31442c6bc53f65d8d6dbbf48be38fea6&auto=format&fit=crop&w=1576&q=80'
+     { name: data.name,
+       temp: data.main.temp,
+       pic: pic,
      }
    ]
  },
 
  //returns array of cities that match the selected option(s) from the user
  cityData() {
-   console.log('QUERY', Template.instance().query.get())
    let query = Template.instance().query.get();
    let icon;
    if(query = 'rain') {
@@ -77,6 +136,14 @@ Template.body.helpers({
 });
 
 Template.body.events({
+
+  // set the current city/centerpoint for the search
+  'submit form': function(event){
+    event.preventDefault();
+    var city = event.target.cityName.value;
+    Template.instance().city.set(city);
+  },
+
   'click .rain': function (event) {
     console.log('you clicked on rain')
     return Template.instance().query.set('rain');
