56dabf4f10b89d4b28bf0730317c1e501969c8ae
vberihuete <vincentberihuete809@gmail.com>
2018-10-30 06:13:18 -0400

    Cleaning project; setting up cocoapods

---
+++ CodeChallenge2/controller/WeatherTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WeatherTableViewController.swift
+//  CodeChallenge2
+//
+//  Created by Vincent Berihuete on 10/30/18.
+//  Copyright © 2018 vincentchallenges. All rights reserved.
+//
+
+import UIKit
+
+class WeatherTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
