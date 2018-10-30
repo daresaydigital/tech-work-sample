210a73d13a240e6df279a7f2f1fbf1e1814acc81
vberihuete <vincentberihuete809@gmail.com>
2018-10-30 06:03:02 -0400

    Initial Commit

---
+++ CodeChallenge2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeChallenge2
+//
+//  Created by Vincent Berihuete on 10/30/18.
+//  Copyright © 2018 vincentchallenges. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
