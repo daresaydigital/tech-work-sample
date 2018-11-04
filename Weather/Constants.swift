5bab6ffe00dc7ec5c96afb557fd7123a39428e85
Christian Huang <christian@christians-imac.local>
2018-11-04 09:37:53 +0700

    Add LocationManager

---
+++ Weather/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Weather
+//
+//  Created by Christian  Huang on 03/11/18.
+//  Copyright © 2018 Christian Huang. All rights reserved.
+//
+
+import Foundation
+
+
+struct Constants {
+    static let apiKey = "<Weather-API-Key>"
+}
