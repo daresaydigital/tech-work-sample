3cfe38ca0f106c5109dd5203b3589bd8d6b56a56
Cem Atilgan <cem@perigee.se>
2020-09-16 15:45:50 +0200

    Initial commit

---
+++ MovieDatabase/MovieDatabase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDatabase
+//
+//  Created by Cem Atilgan on 2020-09-16.
+//  Copyright © 2020 Cem Atilgan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
