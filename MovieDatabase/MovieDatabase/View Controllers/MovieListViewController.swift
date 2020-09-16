e8723e95fa625f2ae3d274a8d4e9a191581cc779
Cem Atilgan <cem@perigee.se>
2020-09-16 16:34:05 +0200

    Add MovieListViewController to the project and to the storyboard

---
+++ MovieDatabase/MovieDatabase/View Controllers/MovieListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MovieListViewController.swift
+//  MovieDatabase
+//
+//  Created by Cem Atilgan on 2020-09-16.
+//  Copyright © 2020 Cem Atilgan. All rights reserved.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
