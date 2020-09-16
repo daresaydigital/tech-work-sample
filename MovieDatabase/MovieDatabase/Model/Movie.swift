5bef68682196b0204e352d2bb21ca7e6ac8085ff
Cem Atilgan <cem@perigee.se>
2020-09-16 16:26:41 +0200

    Create PagedMovieResponse and Movie model objects and restructure the folder structure

---
+++ MovieDatabase/MovieDatabase/Model/Movie.swift
@@ -0,0 +1,33 @@
+//
+//  Movie.swift
+//  MovieDatabase
+//
+//  Created by Cem Atilgan on 2020-09-16.
+//  Copyright © 2020 Cem Atilgan. All rights reserved.
+//
+
+import Foundation
+
+struct Movie: Codable {
+    let id: Int
+    let title: String
+    let overview: String
+    let posterPath: String
+    let backdropPath: String
+    let releaseDate: String
+    let genreIDs: [Int]
+    let numberOfTotalVotes: Int
+    let rating: Double
+
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case title = "original_title"
+        case overview
+        case posterPath = "poster_path"
+        case backdropPath = "backdrop_path"
+        case releaseDate = "release_date"
+        case genreIDs = "genre_ids"
+        case numberOfTotalVotes = "vote_count"
+        case rating = "vote_average"
+    }
+}
---
+++ MovieDatabase/MovieDatabase/Model/PagedMovieResponse.swift
@@ -0,0 +1,23 @@
+//
+//  PagedMovieResponse.swift
+//  MovieDatabase
+//
+//  Created by Cem Atilgan on 2020-09-16.
+//  Copyright © 2020 Cem Atilgan. All rights reserved.
+//
+
+import Foundation
+
+struct PagedMoviesResponse: Codable {
+    let page: Int
+    let totalResults: Int
+    let totalPages: Int
+    let results: [Movie]
+
+    enum CodingKeys: String, CodingKey {
+        case page
+        case totalResults = "total_results"
+        case totalPages = "total_pages"
+        case results
+    }
+}
