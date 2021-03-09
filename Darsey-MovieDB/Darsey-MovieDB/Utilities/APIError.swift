e58c802c350d8c177b4d3077ef2cf98a0a2259b2
Emil Vaklinov <emil_vaklinov@yahoo.com>
2021-03-09 19:46:20 +0000

    Error handling completed

--- Darsey-MovieDB/Darsey-MovieDB/Utilities/APIError.swift
+++ Darsey-MovieDB/Darsey-MovieDB/Utilities/APIError.swift
@@ -14,4 +14,18 @@ enum APIError: Error {
     case invalidData
     case decodeError
     
+    var localizedDescription : String{
+        switch self {
+        case .unknownError:
+            return "error.unknown".localized
+        case .invalidURL:
+            return "error.invalidURL".localized
+        case .invalidResponse:
+            return "error.response".localized
+        case .invalidData:
+            return "error.data".localized
+        case .decodeError:
+            return "error.decode".localized
+        }
+    }
 }
