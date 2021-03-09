084f7eaf36aa0bd141d75556150b6804f9b67755
Emil Vaklinov <emil_vaklinov@yahoo.com>
2021-03-09 16:28:14 +0000

    Test added and some bug fixings

--- Darsey-MovieDB/Darsey-MovieDBTests/Darsey_MovieDBTests.swift
+++ Darsey-MovieDB/Darsey-MovieDBTests/Darsey_MovieDBTests.swift
@@ -10,24 +10,43 @@ import XCTest
 
 class Darsey_MovieDBTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    //MARK: - Properties
+    
+    private var movie: Movie!
+    
+    // MARK: Test lifecycle
+    override func setUp() {
+        movie = Movie(title: "Inception",
+                      release_date: "2010",
+                      vote_average: 9.0,
+                      poster_path: "poster.url.com",
+                      backdrop_path: "backdrop.url.com",
+                      id: 123456)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testMovieViewModel() {
+        let viewModel = MoviesViewModel(movie)
+        
+        XCTAssertEqual(movie.id, viewModel.id)
+        XCTAssertEqual(movie.release_date, viewModel.release_date)
+        XCTAssertEqual(movie.vote_average, viewModel.vote_average)
+        XCTAssertEqual(movie.poster_path, viewModel.poster_path)
+        XCTAssertEqual(movie.backdrop_path, viewModel.backdrop_path)
+        XCTAssertEqual(movie.title, viewModel.title)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testMoviePosterURL(){
+        let movieVM = MoviesViewModel(movie)
+        let mockPosterURL = ApiURL.image + "w342/\(movie.poster_path!)"
+        
+        XCTAssertEqual(mockPosterURL, movieVM.posterURL)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testMovieBackdropURL(){
+        let movieVM = MoviesViewModel(movie)
+        let mockBackdropURL = ApiURL.image + "original/\(movie.backdrop_path!)"
+        
+        XCTAssertEqual(mockBackdropURL, movieVM.backdropURL)
     }
-
+    
 }
