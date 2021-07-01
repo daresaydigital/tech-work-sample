bc300c53c8bda16b2ea946902752c4383f9e8a4f
johannesloor <johannes.loor@gmail.com>
2021-07-01 14:09:12 +0200

    Initial setup

---
+++ DareFilm/DareFilm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DareFilm
+//
+//  Created by Johannes Loor on 2021-07-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DareFilm/DareFilm/DareFilmApp.swift
@@ -0,0 +1,17 @@
+//
+//  DareFilmApp.swift
+//  DareFilm
+//
+//  Created by Johannes Loor on 2021-07-01.
+//
+
+import SwiftUI
+
+@main
+struct DareFilmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
