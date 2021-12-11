ed177add461b9b257c4826165a2f983bf81646b6
AliSani <ali.nj.sani@gmail.com>
2021-12-11 12:06:53 +0330

    Initial commit

---
+++ TheMovieDB/TheMovieDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovieDB
+//
+//  Created by Ali Sani on 12/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
