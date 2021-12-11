fe84154bb3fdd7c660f80c7075d8011a9f19bba8
AliSani <ali.nj.sani@gmail.com>
2021-12-11 21:51:02 +0330

    Add more UITest cases

--- TheMovieDB/TheMovieDBUITests/TheMovieDBUITests.swift
+++ TheMovieDB/TheMovieDBUITests/TheMovieDBUITests.swift
@@ -25,6 +25,17 @@ class TheMovieDBUITests: XCTestCase {
         XCTAssert(app.staticTexts["The Movie DB"].waitForExistence(timeout: 1))
         XCTAssert(app.tables.buttons["Top rated"].waitForExistence(timeout: 1))
         XCTAssert(app.tables.buttons["Popular"].waitForExistence(timeout: 1))
-                                                
+    }
+    
+    // TODO: We should use mocked data here instead.
+    func testMovieDetailGenresIsDisplayedCorrectly() {
+        let app = XCUIApplication()
+        app.launch()
+        let tablesQuery = app.tables
+        tablesQuery.cells.containing(.staticText, identifier:"Sep 30, 2021")
+            .staticTexts["Venom: Let There Be Carnage"]
+            .tap()
+        XCTAssert(tablesQuery/*@START_MENU_TOKEN@*/.cells.staticTexts["Science Fiction, Action, Adventure"]/*[[".cells.staticTexts[\"Science Fiction, Action, Adventure\"]",".staticTexts[\"Science Fiction, Action, Adventure\"]"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/
+                    .waitForExistence(timeout: 3))
     }
 }
