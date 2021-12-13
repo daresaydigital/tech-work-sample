407480a739930dd1e94c306cc246c4585b82943d
Emad Beyrami <emad.beyrami#>
2021-12-13 14:18:44 +0330

    Created the Xcode Project

---
+++ Daresay Movies/Daresay Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Daresay Movies
+//
+//  Created by Emad Bayramy on 12/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
