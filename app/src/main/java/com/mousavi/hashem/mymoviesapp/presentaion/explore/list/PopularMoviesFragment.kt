70b0d4f4cb4ce2dd03ef565c13fc428c8c54a622
Hashem <Hashem.Mousavi.m@gmail.com>
2021-12-25 02:46:10 +0330

    impl details fragment

--- app/src/main/java/com/mousavi/hashem/mymoviesapp/presentaion/explore/list/PopularMoviesFragment.kt
+++ app/src/main/java/com/mousavi/hashem/mymoviesapp/presentaion/explore/list/PopularMoviesFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.mousavi.hashem.mymoviesapp.R
 import com.mousavi.hashem.mymoviesapp.presentaion.BaseFragment
+import com.mousavi.hashem.mymoviesapp.presentaion.explore.details.DetailsFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 
@@ -24,7 +27,21 @@ class PopularMoviesFragment : BaseFragment(R.layout.fragment_popular_movies) {
             )
         },
         onItemClicked = { movie, view ->
-            //TODO()
+            view.transitionName =
+                context?.getString(R.string.transition_name_poster) ?: return@PopularMoviesAdapter
+            val extras =
+                FragmentNavigatorExtras(
+                    view to context!!.getString(R.string.transition_name_poster)
+                )
+
+            findNavController().navigate(
+                resId = R.id.action_popularMoviesFragment_to_detailsFragment,
+                args = Bundle().apply {
+                    putParcelable(DetailsFragment.ARG_MOVIE, movie)
+                },
+                navOptions = null,
+                navigatorExtras = extras
+            )
         }
     )
 
