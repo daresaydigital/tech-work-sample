9c4630b54962bd53bb7ab1fa0886ce856a6aa02e
Hashem <Hashem.Mousavi.m@gmail.com>
2021-12-25 00:37:21 +0330

    move business logic to usecase

--- app/src/main/java/com/mousavi/hashem/mymoviesapp/domain/usecases/GetPopularMovies.kt
+++ app/src/main/java/com/mousavi/hashem/mymoviesapp/domain/usecases/GetPopularMovies.kt
@@ -1,13 +1,46 @@
 package com.mousavi.hashem.mymoviesapp.domain.usecases
 
 import com.mousavi.hashem.common.Either
+import com.mousavi.hashem.mymoviesapp.domain.model.Genres
 import com.mousavi.hashem.mymoviesapp.domain.model.PageData
 import com.mousavi.hashem.mymoviesapp.domain.repository.MoviesRepository
+
 
 class GetPopularMovies(
     private val repository: MoviesRepository,
 ) {
     suspend operator fun invoke(language: String, page: Int): Either<PageData, String> {
-        return repository.getPopularMovies(language, page)
+        return when (val genres = repository.getGenres()) {
+            is Either.Success -> {
+                return when (val popularMovies = repository.getPopularMovies(language, page)) {
+                    is Either.Success -> {
+                        popularMovies.data.movies.forEach { movie ->
+                            movie.genreNames.addAll(mapGenreIdToName(movie.genreIds, genres.data))
+                        }
+                        Either.Success(popularMovies.data)
+                    }
+
+                    is Either.Error -> {
+                        Either.Error(popularMovies.error)
+                    }
+                }
+            }
+            is Either.Error -> {
+                Either.Error(genres.error)
+            }
+        }
+    }
+
+    private fun mapGenreIdToName(list: List<Int>, genres: Genres): List<String> {
+        val genreNames = mutableListOf<String>()
+        genres.genres.forEach { genre ->
+            list.forEach { id ->
+                if (id == genre.id) {
+                    genreNames.add(genre.name)
+                }
+            }
+        }
+        return genreNames
     }
 }
+
