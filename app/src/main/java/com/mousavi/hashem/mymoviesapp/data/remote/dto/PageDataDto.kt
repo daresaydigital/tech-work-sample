eaa3f49f06bc6e7a8718d54987b242f0ea66f2d9
Hashem <Hashem.Mousavi.m@gmail.com>
2021-12-24 15:42:41 +0330

    add Dtos(Data Transfer Objects)

---
+++ app/src/main/java/com/mousavi/hashem/mymoviesapp/data/remote/dto/PageDataDto.kt
@@ -0,0 +1,45 @@
+package com.mousavi.hashem.mymoviesapp.data.remote.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class PageDataDto(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+    val movies: List<MovieDto>,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_results")
+    val totalResults: Int,
+)
+
+data class MovieDto(
+    @SerializedName("adult")
+    val adult: Boolean,
+    @SerializedName("backdrop_path")
+    val backdropPath: String?,
+    @SerializedName("genre_ids")
+    val genreIds: List<Int>,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("original_language")
+    val originalLanguage: String,
+    @SerializedName("original_title")
+    val originalTitle: String,
+    @SerializedName("overview")
+    val overview: String,
+    @SerializedName("popularity")
+    val popularity: Double,
+    @SerializedName("poster_path")
+    val posterPath: String?,
+    @SerializedName("release_date")
+    val releaseDate: String?,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("video")
+    val video: Boolean,
+    @SerializedName("vote_average")
+    val voteAverage: Double,
+    @SerializedName("vote_count")
+    val voteCount: Int,
+)
