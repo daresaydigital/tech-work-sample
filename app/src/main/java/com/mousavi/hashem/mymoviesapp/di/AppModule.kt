75069d8dfa90eaf26d51602b9deae1f6f4c63ac5
Hashem <Hashem.Mousavi.m@gmail.com>
2021-12-24 15:54:09 +0330

    add hilt structure

---
+++ app/src/main/java/com/mousavi/hashem/mymoviesapp/di/AppModule.kt
@@ -0,0 +1,10 @@
+package com.mousavi.hashem.mymoviesapp.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+}
