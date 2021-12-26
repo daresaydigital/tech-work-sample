aab545277859b74622e69aecda9dbe1af00b2f4e
alisamawy1370 <alisamawy1370@gmail.com>
2021-12-26 23:11:03 +0330

    implement movie details repository.

---
+++ data/src/main/java/com/daresaydigital/data/features/movie_details/MovieDetailsRepositoryImpl.kt
@@ -0,0 +1,42 @@
+package com.daresaydigital.data.features.movie_details
+
+import com.daresaydigital.data.features.movie_details.local.MovieDetailsLocalDataSource
+import com.daresaydigital.data.features.movie_details.remote.MovieDetailsRemoteDataSource
+import com.daresaydigital.data.features.movie_details.util.toDomainModel
+import com.daresaydigital.data.util.ApiResult
+import com.daresaydigital.domain.features.movie_details.model.MovieDetailsDomain
+import com.daresaydigital.domain.features.movie_details.repository.MovieDetailsRepository
+import com.daresaydigital.domain.model.Result
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class MovieDetailsRepositoryImpl @Inject constructor(
+    private val remoteDataSource: MovieDetailsRemoteDataSource,
+    private val localDataSource: MovieDetailsLocalDataSource
+) : MovieDetailsRepository{
+
+    companion object {
+        private const val UNKNOWN_API_EXCEPTION = "unknown api exception"
+    }
+
+    override suspend fun getMovieDetails(id: Int): Flow<Result<MovieDetailsDomain>> {
+
+        return flow {
+            localDataSource.getMovieDetailsById(id)?.toDomainModel()?.let {
+                emit(Result.Success(it))
+            }
+
+            val remoteResult = remoteDataSource.getMovieDetails(id)
+            when(remoteResult){
+                is ApiResult.Success -> {
+                    emit(Result.Success(remoteResult.value.toDomainModel()))
+                }
+                is ApiResult.Failure -> {
+                    emit(Result.Failure(remoteResult.error.message ?: UNKNOWN_API_EXCEPTION))
+                }
+            }
+        }
+    }
+
+}
--- domain/src/main/java/com/daresaydigital/domain/features/movie_details/repository/MovieDetailsRepository.kt
+++ domain/src/main/java/com/daresaydigital/domain/features/movie_details/repository/MovieDetailsRepository.kt
@@ -1,7 +1,9 @@
 package com.daresaydigital.domain.features.movie_details.repository
 
-import com.daresaydigital.domain.model.MovieDomain
+import com.daresaydigital.domain.features.movie_details.model.MovieDetailsDomain
+import com.daresaydigital.domain.model.Result
+import kotlinx.coroutines.flow.Flow
 
 interface MovieDetailsRepository {
-    suspend fun getMovieDetails(id:Int): Result<MovieDomain>
+    suspend fun getMovieDetails(id:Int): Flow<Result<MovieDetailsDomain>>
 }
