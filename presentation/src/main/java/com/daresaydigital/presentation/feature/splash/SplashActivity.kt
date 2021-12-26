ee12771b6b7d17abd58f5e8e1a6d3b2f29b38dc2
alisamawy1370 <alisamawy1370@gmail.com>
2021-12-26 11:59:05 +0330

    init splash activity.

--- presentation/src/main/java/com/daresaydigital/presentation/feature/splash/SplashActivity.kt
+++ presentation/src/main/java/com/daresaydigital/presentation/feature/splash/SplashActivity.kt
@@ -1,8 +1,11 @@
-package com.daresaydigital.tmdb
+package com.daresaydigital.presentation.feature.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.daresaydigital.presentation.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
