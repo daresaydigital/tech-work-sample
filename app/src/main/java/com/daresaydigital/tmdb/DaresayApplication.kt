a7cc4733dcd0b703e2e33a9ca3921156bc631678
alisamawy1370 <alisamawy1370@gmail.com>
2021-12-26 11:58:28 +0330

    implement resources and application class in app module.

---
+++ app/src/main/java/com/daresaydigital/tmdb/DaresayApplication.kt
@@ -0,0 +1,10 @@
+package com.daresaydigital.tmdb
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DaresayApplication : Application() {
+
+    //set config here
+}
