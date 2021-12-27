7af22737d5aa57f5cef30e2a18cea548b307a456
Ramy Atalla <ramywadia@gmail.com>
2021-12-27 14:55:18 +0100

    Creating TabBarController

--- MovieShow/MovieShow/Support/SceneDelegate.swift
+++ MovieShow/MovieShow/Support/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: scene.coordinateSpace.bounds)
         window?.windowScene = scene
-        window?.rootViewController = ViewController()
+        window?.rootViewController = MovieTabBarController()
         window?.makeKeyAndVisible()
     }
 
---
+++ MovieShow/MovieShow/Utils/K.swift
@@ -0,0 +1,15 @@
+//
+//  K.swift
+//  MovieShow
+//
+//  Created by Ramy Atalla on 2021-12-27.
+//
+
+import UIKit
+
+enum SFSymbols {
+    static let film = UIImage(systemName: "film")
+    static let filmSelected = UIImage(systemName: "film.fill")
+    static let downloaded = UIImage(systemName: "arrow.down.circle")
+    static let downloadedSelected = UIImage(systemName: "arrow.down.circle.fill")
+}
