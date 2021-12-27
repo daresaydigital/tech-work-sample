0ede02aac16c3bab23404ac1cbdf36095e7e92d0
alisamawy1370 <alisamawy1370@gmail.com>
2021-12-27 22:23:30 +0330

    implement top rated ui and logics.

--- presentation/src/main/java/com/daresaydigital/presentation/feature/main/home/top_rated/TopRatedMovieFragment.kt
+++ presentation/src/main/java/com/daresaydigital/presentation/feature/main/home/top_rated/TopRatedMovieFragment.kt
@@ -1,9 +1,18 @@
 package com.daresaydigital.presentation.feature.main.home.top_rated
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.daresaydigital.domain.model.Movie
 import com.daresaydigital.presentation.R
 import com.daresaydigital.presentation.base.BaseFragment
+import com.daresaydigital.presentation.feature.main.home.MovieAdapter
+import com.daresaydigital.presentation.util.extensions.observeNullSafe
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_top_rated.*
 
 @AndroidEntryPoint
 class TopRatedMovieFragment : BaseFragment<TopRatedMovieViewModel>(){
@@ -12,4 +21,91 @@ class TopRatedMovieFragment : BaseFragment<TopRatedMovieViewModel>(){
 
     override fun layoutId(): Int = R.layout.fragment_top_rated
 
+    private var movieAdapter : MovieAdapter? = null
+    private var layoutManager : GridLayoutManager? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupViews()
+        setupObservers()
+
+        viewModel.getTopRatedPage()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        movieAdapter = null
+    }
+
+    private fun setupViews(){
+        movieAdapter = MovieAdapter(mutableListOf()) { movie -> adapterOnClick(movie) }
+        layoutManager = GridLayoutManager(requireContext(),2)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = movieAdapter
+        recyclerView.addOnScrollListener(recyclerViewOnScrollListener)
+    }
+
+    private fun setupObservers(){
+        viewModel.progressVisibilityLiveData.observeNullSafe(viewLifecycleOwner){
+            if (it.first){
+                if (it.second == 1) {
+                    frmLoading.visibility = View.VISIBLE
+                } else {
+                    bottomProgressbar.visibility = View.VISIBLE
+                }
+            }
+            else {
+                frmLoading.visibility = View.GONE
+                bottomProgressbar.visibility = View.GONE
+            }
+        }
+
+        viewModel.failureEventLiveData.observeNullSafe(viewLifecycleOwner){ pair ->
+            viewFailureError(pair.first ?: getString(R.string.something_went_wrong)) {
+                viewModel.getTopRatedPage(pair.second == 1)
+            }
+        }
+
+        val currentMovieList = mutableListOf<Movie>()
+        viewModel.movieListLiveData.observeNullSafe(viewLifecycleOwner){ pair ->
+            pair.first?.let {
+                if (pair.second == 1) {
+                    currentMovieList.clear()
+                }
+                currentMovieList.addAll(it)
+                movieAdapter?.setData(currentMovieList)
+            }
+        }
+    }
+
+    private fun viewFailureError(message: String, listener: View.OnClickListener) {
+        val snackBar = Snackbar.make(
+            root,
+            message, Snackbar.LENGTH_LONG
+        )
+        snackBar.setAction(R.string.retry, listener)
+        snackBar.show()
+    }
+
+    private fun adapterOnClick(movie: Movie) {
+        //todo
+    }
+
+    private val PAGE_SIZE = 20
+    private val recyclerViewOnScrollListener: RecyclerView.OnScrollListener =
+        object : RecyclerView.OnScrollListener() {
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val visibleItemCount: Int = layoutManager?.childCount ?: 0
+                val totalItemCount: Int = layoutManager?.itemCount ?: 0
+                val firstVisibleItemPosition: Int = layoutManager?.findFirstVisibleItemPosition() ?: 0
+                if (viewModel.progressVisibilityLiveData.value?.first == false && !viewModel.checkIsLastPage()) {
+                    if (visibleItemCount + firstVisibleItemPosition >= totalItemCount && firstVisibleItemPosition >= 0 && totalItemCount >= PAGE_SIZE) {
+                        viewModel.getTopRatedPage(false)
+                    }
+                }
+            }
+        }
 }
--- presentation/src/main/java/com/daresaydigital/presentation/feature/main/home/top_rated/TopRatedMovieViewModel.kt
+++ presentation/src/main/java/com/daresaydigital/presentation/feature/main/home/top_rated/TopRatedMovieViewModel.kt
@@ -1,9 +1,80 @@
 package com.daresaydigital.presentation.feature.main.home.top_rated
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.daresaydigital.core.utils.GlobalDispatcher
+import com.daresaydigital.domain.features.top_rated_movie.usecase.GetTopRatedMoviesParams
+import com.daresaydigital.domain.features.top_rated_movie.usecase.GetTopRatedMoviesUseCase
+import com.daresaydigital.domain.model.Movie
+import com.daresaydigital.domain.model.Result
 import com.daresaydigital.presentation.base.BaseViewModel
+import com.daresaydigital.presentation.util.SingleLiveEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class TopRatedMovieViewModel @Inject constructor() : BaseViewModel(){
+class TopRatedMovieViewModel @Inject constructor(
+    private val getTopRatedMoviesUseCase: GetTopRatedMoviesUseCase,
+    private val globalDispatcher: GlobalDispatcher
+) : BaseViewModel(){
+
+    private val _progressVisibilityLiveData = MutableLiveData<Pair<Boolean,Int>>()
+    val progressVisibilityLiveData: LiveData<Pair<Boolean, Int>> = _progressVisibilityLiveData
+
+    private val _failureEventLiveData = SingleLiveEvent<Pair<String?,Int>>()
+    val failureEventLiveData: LiveData<Pair<String?, Int>> = _failureEventLiveData
+
+    private val _movieListLiveData = MutableLiveData<Pair<List<Movie>?,Int>>()
+    val movieListLiveData: LiveData<Pair<List<Movie>?, Int>> = _movieListLiveData
+
+    private var currentPageNumber = 1
+    private var totalPageNumber = 1
+
+    fun getTopRatedPage(isRefresh:Boolean = true){
+
+        if (isRefresh) {
+            currentPageNumber = 1
+            totalPageNumber = 1
+        } else {
+            currentPageNumber ++
+        }
+
+        viewModelScope.launch(globalDispatcher.main) {
+            setPageLoading(true)
+            if (currentPageNumber == 1 || currentPageNumber < totalPageNumber){
+                when(
+                    val result = getTopRatedMoviesUseCase.execute(GetTopRatedMoviesParams(currentPageNumber))
+                ){
+                    is Result.Success -> {
+                        totalPageNumber = result.value.totalPages
+                        currentPageNumber = result.value.page
+                        handleDataSucceed(result.value.results)
+                    }
+                    is Result.Failure -> {
+                        handleDataFailure(result.error)
+                    }
+                }
+            }
+            setPageLoading(false)
+        }
+    }
+
+    private fun setPageLoading(isLoading: Boolean) {
+        _progressVisibilityLiveData.value = Pair(isLoading,currentPageNumber)
+    }
+
+    private fun handleDataFailure(error: String) {
+        _failureEventLiveData.value = Pair(error,currentPageNumber)
+    }
+
+    private fun handleDataSucceed(movies: List<Movie>) {
+        _movieListLiveData.value = Pair(movies,currentPageNumber)
+    }
+
+    fun checkIsLastPage(): Boolean {
+        return currentPageNumber == totalPageNumber
+    }
+
 }
