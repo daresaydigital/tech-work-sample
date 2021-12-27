307282dbe106191d70710babd7b3bfadb3c45786
Ramy Atalla <ramywadia@gmail.com>
2021-12-27 14:36:22 +0100

    Initializing the app with No Storyboard

---
+++ MovieShow/MovieShow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieShow
+//
+//  Created by Ramy Atalla on 2021-12-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
