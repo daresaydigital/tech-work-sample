6694bebe0af8a147988a72f8d66de4472588c35b
Emran <emran@Emrans-MacBook-Pro-16-512G.local>
2022-01-02 21:45:54 +0330

    Add Nuke Pod for download images

---
+++ DareMovie/Pods/Target Support Files/Pods-DareMovie-DareMovieUITests/Pods-DareMovie-DareMovieUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_DareMovie_DareMovieUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_DareMovie_DareMovieUITestsVersionString[];
+
