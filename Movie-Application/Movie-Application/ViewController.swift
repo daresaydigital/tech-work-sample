05400415b34b971783d13a789234dbc46ef79196
mohannazakizadeh <mohannazakizadeh@gmail.com>
2022-04-23 13:02:05 +0430

    Viper: Created project

---
+++ Movie-Application/Movie-Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie-Application
+//
+//  Created by Mohanna Zakizadeh on 4/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
