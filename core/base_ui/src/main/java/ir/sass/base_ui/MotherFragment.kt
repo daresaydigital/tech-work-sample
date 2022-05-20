f10ec969488c55f149e1ccda4a49041852d2a165
drflakelorenzgerman <Helloiamsass1>
2022-05-20 19:11:11 +0430

    title : base ui description : 1.create MotherFragment.kt and it's functions 2.add json lottie for loading 3.add colors

---
+++ core/base_ui/src/main/java/ir/sass/base_ui/MotherFragment.kt
@@ -0,0 +1,73 @@
+package ir.sass.base_ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import ir.sass.base_ui.databinding.FragmentBaseBinding
+import kotlinx.coroutines.launch
+
+
+abstract class MotherFragment<DataBinding : ViewDataBinding>(
+    private val setting : MotherFragmentSetting
+) : Fragment(R.layout.fragment_base) {
+
+    private lateinit var dataBindingOuter : FragmentBaseBinding
+    protected lateinit var dataBinding : DataBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        dataBindingOuter = FragmentBaseBinding.inflate(inflater)
+        dataBindingOuter.title = setting.title
+        return dataBindingOuter.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dataBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(requireContext()),setting.layout,dataBindingOuter.linAdd,true
+        )
+        binding()
+    }
+
+    /*you will write your code here by overriding this function*/
+    abstract fun binding()
+
+
+    /*2 functions for handling loading*/
+    fun loadingOn(){
+        dataBindingOuter.loading = true
+    }
+
+    fun loadingOff(){
+        dataBindingOuter.loading = false
+    }
+
+
+    /*you can handle your suspend functions here,it takes one parameter which is named
+    state and that's from Lifecycle.State, you will pass your action and the
+    action will be invoked*/
+    fun coroutinesLauncher(state : Lifecycle.State, action :suspend () -> Unit){
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(state){
+                action.invoke()
+            }
+        }
+    }
+}
+
+class MotherFragmentSetting(
+    @LayoutRes
+    val layout : Int,
+    val title : String
+)
