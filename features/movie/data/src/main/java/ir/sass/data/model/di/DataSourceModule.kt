b186a18d17f6f828eba903c105a6b23bea72657a
drflakelorenzgerman <Helloiamsass1>
2022-05-21 12:37:38 +0430

    title : movie feature description : 1.add global query param as api_key

--- features/movie/data/src/main/java/ir/sass/data/model/di/DataSourceModule.kt
+++ features/movie/data/src/main/java/ir/sass/data/model/di/DataSourceModule.kt
@@ -5,6 +5,9 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import ir.sass.data.model.datasource.remote.DiscoverMovieApi
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -13,10 +16,26 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object DataSourceModule {
     @Provides
+    fun provideOkHttpClient() : OkHttpClient {
+        val okClient = OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                var request: Request = chain.request()
+                val url: HttpUrl = request.url().newBuilder()
+                    .addQueryParameter("api_key", "500b07a7f284efab4476a03d5fc384e1")
+                    .build()
+                request = request.newBuilder().url(url).build()
+                chain.proceed(request)
+            }
+            .build()
+        return okClient
+    }
+
+    @Provides
     @Singleton
-    fun provideRetrofit() : Retrofit = Retrofit.Builder()
+    fun provideRetrofit(httpClient : OkHttpClient) : Retrofit = Retrofit.Builder()
         .baseUrl("https://api.themoviedb.org/")
         .addConverterFactory(GsonConverterFactory.create())
+        .client(httpClient)
         .build()
 
     @Provides
