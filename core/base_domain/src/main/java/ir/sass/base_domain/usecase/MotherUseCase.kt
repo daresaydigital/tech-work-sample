fc58f80a36cf431d7cb530b9adcd1d6a21cacba8
drflakelorenzgerman <Helloiamsass1>
2022-05-21 12:03:46 +0430

    title : base domain description : 1.add Mother Usecase for base of other use cases

---
+++ core/base_domain/src/main/java/ir/sass/base_domain/usecase/MotherUseCase.kt
@@ -0,0 +1,18 @@
+package ir.sass.base_domain.usecase
+
+import ir.sass.basedomain.model.Domain
+import kotlinx.coroutines.flow.Flow
+
+abstract class MotherUseCase<in Input, Output>{
+    abstract operator fun invoke(input : Input) : Flow<Domain<Output>>
+}
+
+abstract class MotherUseCaseWithOnlyInput<in Input>(){
+    abstract operator fun invoke(input : Input)
+}
+
+abstract class MotherUseCaseWithOnlyOutput<Output>{
+    abstract operator fun invoke() : Flow<Domain<Output>>
+}
+
+
