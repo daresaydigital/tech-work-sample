c55ff51c25196ed5b2596e6497535e10b703a915
drflakelorenzgerman <Helloiamsass1>
2022-05-21 13:04:55 +0430

    title : base data description : 1.add test for safeApi

---
+++ core/base_data/src/test/java/ir/sass/base_data/SafeApiTest.kt
@@ -0,0 +1,28 @@
+package ir.sass.base_data
+
+import ir.sass.base_data.test.BaseDataTest
+import ir.sass.basedomain.model.Domain
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class SafeApiTest : BaseDataTest(){
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `check if the safeApi can return the correct data when the request is success`() = testScope.runTest {
+        val data : Domain.Data<Int> = helperSuccess()
+        assert(data.data.isSuccess && data.data.getOrThrow() == 1)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `check if the safeApi can return the correct data when the request is not success`() = testScope.runTest{
+        val data : Domain.Data<Int> = helperFailed()
+        assert(data.data.isFailure && data.data.exceptionOrNull()!!.message!! == "Error")
+    }
+
+    suspend fun helperSuccess() : Domain.Data<Int> = Domain.Data(Result.success(1))
+
+    suspend fun helperFailed() : Domain.Data<Int> = Domain.Data(Result.failure(Throwable("Error")))
+}
