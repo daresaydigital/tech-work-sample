94b1e00c99b6c1aa2bcd2c892e6bb909d1f48438
drflakelorenzgerman <Helloiamsass1>
2022-05-21 12:58:48 +0430

    title : base data description : 1.add test for mapper

---
+++ core/base_data/src/test/java/ir/sass/base_data/MapperTest.kt
@@ -0,0 +1,22 @@
+package ir.sass.base_data
+
+import ir.sass.base_data.model.toJsonString
+import ir.sass.base_data.model.toReal
+import org.junit.Test
+
+class MapperTest {
+    data class A(val number : Int,val word : String)
+    @Test
+    fun `check if the mapper can cast class A to jsonString`(){
+        val a = A(1,"google")
+        val jsonString = toJsonString(a)
+        assert(jsonString.equals("{\"number\":1,\"word\":\"google\"}"))
+    }
+
+    @Test
+    fun `check if the mapper can cast json string to the type of A`(){
+        val json = "{\"number\":1,\"word\":\"google\"}"
+        val casted = toReal<A>(json)
+        assert(casted.number == 1 && casted.word == "google")
+    }
+}
