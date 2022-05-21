198cca4a2c8b17e723ae2fc8cb07541b02dde2be
drflakelorenzgerman <Helloiamsass1>
2022-05-21 13:34:06 +0430

    title : home module description : 1.create Homefragment

---
+++ features/home/ui/src/main/java/ir/sass/ui/home/HomeFragment.kt
@@ -0,0 +1,14 @@
+package ir.sass.ui.home
+
+import ir.sass.base_ui.MotherFragment
+import ir.sass.base_ui.MotherFragmentSetting
+import ir.sass.ui.R
+import ir.sass.ui.databinding.FragmentHomeBinding
+
+class HomeFragment : MotherFragment<FragmentHomeBinding>(
+    MotherFragmentSetting(R.layout.fragment_home,"Home")
+) {
+    override fun binding() {
+
+    }
+}
