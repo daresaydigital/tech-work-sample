9105580abb6693f4c9d380efd0f69a78b655a1b9
Kristian Skog <skogkristian@gmail.com>
2022-10-08 18:35:09 +0200

    Added functionality to load more movies

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,10 @@
 import client from "./client";
 
-const BASE_URL = `https://api.themoviedb.org/3/movie/popular`;
+const BASE_URL = `https://api.themoviedb.org/3/`;
+const URL_PATH = `movie/popular`;
 
-const getMovies = async () => {
-  return await client(BASE_URL);
+const getMovies = async (pageNumber = 1) => {
+  return await client(`${BASE_URL}${URL_PATH}?page=${pageNumber}`);
 };
 
 export { getMovies };
--- src/components/MovieGrid.jsx
+++ src/components/MovieGrid.jsx
@@ -3,35 +3,62 @@ import { getMovies } from "../api";
 
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Unstable_Grid2";
+import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
 
 import MovieCard from "./MovieCard";
 
 export const MovieGrid = () => {
   const [movies, setMovies] = useState();
+  const [pageNumber, setPageNumber] = useState(1);
+  const [maxPageNumber, setMaxPageNumber] = useState();
+  const [loading, setLoading] = useState(false);
 
-  async function requestMovies() {
-    const moviesResponse = await getMovies();
-    setMovies(moviesResponse);
+  async function requestMovies(pageNumber) {
+    setLoading(true);
+    const moviesResponse = await getMovies(pageNumber);
+    setLoading(false);
+    setMaxPageNumber(moviesResponse.total_pages);
+    if (movies) {
+      setMovies((movies) => [...movies, ...moviesResponse.results]);
+    } else {
+      setMovies(moviesResponse.results);
+    }
   }
 
   useEffect(() => {
-    requestMovies();
-  }, []);
+    requestMovies(pageNumber);
+  }, [pageNumber]);
+
+  const loadMore = () => setPageNumber((pageNumber) => pageNumber + 1);
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={{ xs: 2, md: 3 }}>
-        {movies &&
-          movies.results.map((movie, index) => (
-            <Grid xs={6} sm={4} md={3} key={index}>
-              <MovieCard
-                key={`${index}-${movie.original_title}`}
-                movieInformation={movie}
-                index={index}
-              />
-            </Grid>
-          ))}
-      </Grid>
+      {loading && pageNumber === 1 ? (
+        <CircularProgress size={40} thickness={4} value={100} disableShrink />
+      ) : (
+        <Grid container spacing={{ xs: 2, md: 3 }}>
+          {movies &&
+            movies.map((movie, index) => (
+              <Grid xs={6} sm={4} md={3} key={index}>
+                <MovieCard
+                  key={`${index}-${movie.original_title}`}
+                  movieInformation={movie}
+                  index={index}
+                />
+              </Grid>
+            ))}
+        </Grid>
+      )}
+
+      {loading && (pageNumber !== 1 || pageNumber <= maxPageNumber) && (
+        <CircularProgress size={40} thickness={4} value={100} disableShrink />
+      )}
+      {pageNumber <= maxPageNumber && (
+        <Button variant="contained" onClick={loadMore}>
+          Load more
+        </Button>
+      )}
     </Box>
   );
 };
