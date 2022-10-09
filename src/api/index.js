39446d98b207ef1c7090d3075a050254f6f62563
Kristian Skog <skogkristian@gmail.com>
2022-10-09 23:19:34 +0200

    Removed console.log from api/index.js

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,6 @@ import client from "./client";
 const BASE_URL = `https://api.themoviedb.org/3/`;
 
 const getMovies = async (pageNumber = 1, fetchMoviePath) => {
-  console.log(`${BASE_URL}movie/${fetchMoviePath}?page=${pageNumber}`);
   return await client(`${BASE_URL}movie/${fetchMoviePath}?page=${pageNumber}`);
 };
 
