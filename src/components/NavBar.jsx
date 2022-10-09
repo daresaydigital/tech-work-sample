1478956d32840716989dcc580fa5d45794811a02
Kristian Skog <skogkristian@gmail.com>
2022-10-09 23:20:39 +0200

    Updated Navbar code and design

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,56 @@
-import { Container } from "@mui/material";
-import { Link } from "react-router-dom";
+import { AppBar, Box } from "@mui/material";
+import { Container } from "@mui/system";
 
-export const NavBar = () => (
-  <Container>
-    <Link to={`/`}>Lint</Link>
-  </Container>
-);
+import { NavLink } from "react-router-dom";
+
+export const NavBar = () => {
+  const headerData = [
+    {
+      label: "Popular movies",
+      path: "/",
+    },
+    {
+      label: "Top rated movies",
+      path: "/top_rated",
+    },
+  ];
+
+  const navBarStyling = {
+    flexGrow: 1,
+
+    a: {
+      textDecoration: "none",
+      color: "white",
+      padding: "10px",
+      fontSize: { xs: "20px", md: "26px" },
+
+      "&:hover": {
+        textDecoration: "underline",
+      },
+    },
+  };
+
+  let activeStyle = {
+    color: "black",
+    textDecoration: "underline",
+  };
+
+  return (
+    <Box sx={navBarStyling}>
+      <AppBar position="static" color="primary">
+        <Container sx={{ mt: 4, mb: 4 }}>
+          {headerData.map(({ label, path, index }) => (
+            <NavLink
+              key={index}
+              to={path}
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+              end
+            >
+              {label}
+            </NavLink>
+          ))}
+        </Container>
+      </AppBar>
+    </Box>
+  );
+};
