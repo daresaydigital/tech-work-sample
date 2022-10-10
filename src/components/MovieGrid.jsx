7c7c12480edd31bfd701991d01edd4dde35a6b39
Kristian Skog <skogkristian@gmail.com>
2022-10-10 07:25:14 +0200

    Movie grid code update and added container

--- src/components/MovieGrid.jsx
+++ src/components/MovieGrid.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 
+import MovieCard from "./MovieCard";
+
+import { Container } from "@mui/material";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Unstable_Grid2";
 import Button from "@mui/material/Button";
 import CircularProgress from "@mui/material/CircularProgress";
-
-import MovieCard from "./MovieCard";
 
 export const MovieGrid = ({ fetchMovies, fetchMoviePath }) => {
   const [movies, setMovies] = useState();
@@ -14,7 +15,7 @@ export const MovieGrid = ({ fetchMovies, fetchMoviePath }) => {
   const [loading, setLoading] = useState(false);
   const [fetchPrevPath, setfetchPrevPath] = useState();
 
-  async function requestMovies(pageNumber) {
+  const requestMovies = async (pageNumber) => {
     setLoading(true);
     const moviesResponse = await fetchMovies(pageNumber, fetchMoviePath);
     setLoading(false);
@@ -25,7 +26,7 @@ export const MovieGrid = ({ fetchMovies, fetchMoviePath }) => {
       setMovies(moviesResponse.results);
       setfetchPrevPath(fetchMoviePath);
     }
-  }
+  };
 
   useEffect(() => {
     requestMovies(pageNumber);
@@ -35,42 +36,54 @@ export const MovieGrid = ({ fetchMovies, fetchMoviePath }) => {
   const loadMore = () => setPageNumber((pageNumber) => pageNumber + 1);
 
   return (
-    <Box sx={{ flexGrow: 1, mt: 7 }}>
-      {loading && pageNumber === 1 ? (
-        <Grid container justifyContent="center">
-          <CircularProgress size={50} thickness={4} value={100} disableShrink />
-        </Grid>
-      ) : (
-        <Grid container spacing={{ xs: 2, md: 3 }}>
-          {movies &&
-            movies.map((movie, index) => (
-              <Grid xs={6} sm={4} md={3} key={index}>
-                <MovieCard
-                  key={`${index}-${movie.original_title}`}
-                  movieInformation={movie}
-                  index={index}
-                />
-              </Grid>
-            ))}
-        </Grid>
-      )}
+    <Container>
+      <Box sx={{ flexGrow: 1, mt: 7 }}>
+        {loading && pageNumber === 1 ? (
+          <Grid container justifyContent="center">
+            <CircularProgress
+              size={50}
+              thickness={4}
+              value={100}
+              disableShrink
+            />
+          </Grid>
+        ) : (
+          <Grid container spacing={{ xs: 2, md: 3 }}>
+            {movies &&
+              movies.map((movie, index) => (
+                <Grid xs={6} sm={4} md={3} key={index}>
+                  <MovieCard
+                    key={`${index}-${movie.original_title}`}
+                    movieInformation={movie}
+                    index={index}
+                  />
+                </Grid>
+              ))}
+          </Grid>
+        )}
 
-      {loading && (pageNumber !== 1 || pageNumber <= maxPageNumber) && (
-        <Grid container justifyContent="center" sx={{ m: 5, mb: 7 }}>
-          <CircularProgress size={50} thickness={4} value={100} disableShrink />
-        </Grid>
-      )}
-      {pageNumber <= maxPageNumber && !loading && (
-        <Grid container justifyContent="center" sx={{ m: 5, mb: 7 }}>
-          <Button
-            variant="contained"
-            onClick={loadMore}
-            sx={{ fontSize: "20px" }}
-          >
-            Load more
-          </Button>
-        </Grid>
-      )}
-    </Box>
+        {loading && (pageNumber !== 1 || pageNumber <= maxPageNumber) && (
+          <Grid container justifyContent="center" sx={{ m: 5, mb: 7 }}>
+            <CircularProgress
+              size={50}
+              thickness={4}
+              value={100}
+              disableShrink
+            />
+          </Grid>
+        )}
+        {pageNumber <= maxPageNumber && !loading && (
+          <Grid container justifyContent="center" sx={{ m: 5, mb: 7 }}>
+            <Button
+              variant="contained"
+              onClick={loadMore}
+              sx={{ fontSize: "20px" }}
+            >
+              Load more
+            </Button>
+          </Grid>
+        )}
+      </Box>
+    </Container>
   );
 };
