793ab86661273cbc4cd61a2794356e4231c90162
Kristian Skog <skogkristian@gmail.com>
2022-10-10 07:28:11 +0200

    Small code update Error Page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,17 +1,23 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
+
+import { Container } from "@mui/material";
 
 const ErrorPage = () => {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <Container>
+      <div>
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </div>
+
+      <Link to={"/"}>Go back to start page!</Link>
+    </Container>
   );
 };
 
