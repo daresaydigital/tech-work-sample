e8ab8b432c06d26adb3290819041d862c4f66370
Kristian Skog <skogkristian@gmail.com>
2022-10-10 07:27:13 +0200

    Updated LoadingIndicator design

--- src/components/LoadingIndicator.jsx
+++ src/components/LoadingIndicator.jsx
@@ -1,1 +1,10 @@
-export const LoadingIndicator = () => <p>Loading</p>;
+import Grid from "@mui/material/Unstable_Grid2";
+import CircularProgress from "@mui/material/CircularProgress";
+
+export const LoadingIndicator = () => {
+  return (
+    <Grid container justifyContent="center">
+      <CircularProgress size={50} thickness={4} value={100} disableShrink />
+    </Grid>
+  );
+};
