e0978e2a95e704260e9f892e8b9c862392242c72
Caio Ambrosio <ambrosio.caio13@gmail.com>
2023-01-10 19:48:22 -0300

    Add basic project structure from Xcode

---
+++ MyMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovies
+//
+//  Created by Caio dos Santos Ambrosio on 1/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
