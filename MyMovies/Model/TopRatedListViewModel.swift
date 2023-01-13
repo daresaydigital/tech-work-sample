73db253be09dd88c13720b93ee3169ff468d7010
Caio Ambrosio <ambrosio.caio13@gmail.com>
2023-01-13 18:16:27 -0300

    Create TopRatedListViewModel and also delete TopRatedViewController

---
+++ MyMovies/Model/TopRatedListViewModel.swift
@@ -0,0 +1,64 @@
+//
+//  TopRatedListViewModel.swift
+//  MyMovies
+//
+//  Created by Caio dos Santos Ambrosio on 1/11/23.
+//
+
+import UIKit
+
+class TopRatedListViewModel {
+
+    // MARK: - Properties
+
+    private var topRated: [TrendingViewModel] = []
+    private var selected = 0
+
+    private let apiLoader: APILoader<TopRatedAPI>
+
+    var numberOfRowsInSection: Int {
+        self.topRated.count
+    }
+
+    var titlePage: String = "Top Rated"
+
+    // MARK: - Initializer
+
+    init(apiLoader: APILoader<TopRatedAPI>) {
+        self.apiLoader = apiLoader
+    }
+
+    // MARK: - Functions
+
+    func fetchTopRated(
+        completion: @escaping (TopRatedListViewModel?, ServiceError?) -> ()
+    ) {
+        apiLoader.loadAPIRequest(requestData: nil) { [weak self] topRatedResponse, error in
+            guard let self = self else {
+                return
+            }
+
+            if let _ = error {
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+            } else if let topRatedResponse = topRatedResponse {
+                self.topRated = topRatedResponse.results?.map({ topRated in
+                    TrendingViewModel(trending: topRated)
+                }) ?? []
+
+                DispatchQueue.main.async {
+                    completion(self, nil)
+                }
+            } else {
+                DispatchQueue.main.async {
+                    completion(nil, ServiceError(message: "Service Error: Unknow Error"))
+                }
+            }
+        }
+    }
+
+    func getTrending(_ index: Int) -> TrendingViewModel {
+        return self.topRated[index]
+    }
+}
