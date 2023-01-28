41788c64aba3971620d24a94a5bbe2b12a385cca
Farzaneh <f.givehchian@sdb247.com>
2023-01-28 11:56:55 +0330

    restructure project and add json sample files

---
+++ DarsayTechTest/Models/Error.swift
@@ -0,0 +1,28 @@
+//
+//  Error.swift
+//  DarsayTechTest
+//
+//  Created by Farzaneh on 11/8/1401 AP.
+//
+
+import Foundation
+
+struct AppError: Error, LocalizedError {
+    
+    let reason: String?
+    
+    var errorDescription: String? {
+        reason ?? "Unknown Error"
+    }
+}
+
+enum NetworkError: Error {
+    case badURL
+    case invalidJSON
+    case serverError
+    case noResponse
+    case unauthorized
+    case generalError
+    case decodeFailed
+    case notFound
+}
---
+++ DarsayTechTestUITests/DarsayTechTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DarsayTechTestUITestsLaunchTests.swift
+//  DarsayTechTestUITests
+//
+//  Created by Farzaneh on 11/8/1401 AP.
+//
+
+import XCTest
+
+final class DarsayTechTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
