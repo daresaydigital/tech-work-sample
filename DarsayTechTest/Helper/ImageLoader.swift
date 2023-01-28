a9bf539ee7d9a0efce64d361fc7f370d932ac5ff
Farzaneh <f.givehchian@sdb247.com>
2023-01-28 12:49:16 +0330

    fixed lint of imageloader

--- DarsayTechTest/Helper/ImageLoader.swift
+++ DarsayTechTest/Helper/ImageLoader.swift
@@ -5,4 +5,27 @@
 //  Created by Farzaneh on 11/8/1401 AP.
 //
 
+import Combine
 import Foundation
+import UIKit
+
+final class ImageLoader {
+    static let shared = ImageLoader()
+
+    private lazy var backgroundQueue: OperationQueue = {
+        let queue = OperationQueue()
+        queue.maxConcurrentOperationCount = 5
+        return queue
+    }()
+
+    func loadImage(from url: URL?) -> AnyPublisher<UIImage?, Never> {
+        guard let url else { return Just(nil).eraseToAnyPublisher() }
+       
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map { (data, _) -> UIImage? in return UIImage(data: data) }
+            .catch { _ in return Just(nil) }
+            .subscribe(on: backgroundQueue)
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+}
