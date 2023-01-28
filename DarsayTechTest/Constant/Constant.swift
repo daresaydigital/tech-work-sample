ebbf363dd1a31f1c04fcfd40a0c61cfe8a5e64d3
Farzaneh <f.givehchian@sdb247.com>
2023-01-28 12:45:26 +0330

    constant file added

---
+++ DarsayTechTest/Constant/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  DarsayTechTest
+//
+//  Created by Farzaneh on 11/8/1401 AP.
+//
+
+import Foundation
+
+enum Constant {
+    static let imageBaseURL = "https://image.tmdb.org/t/p/"
+}
